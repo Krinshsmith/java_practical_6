1c7e2bfc90a7c799b2d8d2795845bfcff51af23c
Krinshsmith Kava <141899936+Krinshsmith@users.noreply.github.com>
2024-05-01 22:55:54 +0530

    Create Program-2.java
    Write a program in Java to create a file and perform
    write operation on this file.

---
+++ Program-2.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program-2 {
+    public static void main(String[] args) {
+        System.out.println("My Name Is Krinshsmith Kava");
+        System.out.println("Er_no :: 220130318102");
+
+        try {
+            File file = new File("newFile.txt");
+            if (file.createNewFile()) {
+                System.out.println("The new file is created.");
+            } else {
+                System.out.println("The file already exists.");
+            }
+            String data = "This is the data in the new file.";
+            FileWriter writer = new FileWriter("newFile.txt");
+            writer.write(data);
+            System.out.println("Data is written to the file.");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
